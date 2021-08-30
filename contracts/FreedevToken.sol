a35a1365d9cdb89f4cfe442e99e841785acc9dac
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2021-08-30 20:53:23 +0100

    erc20 token constructor

--- contracts/FreedevToken.sol
+++ contracts/FreedevToken.sol
@@ -18,5 +18,19 @@ interface ERC20Interface {
 
 contract FreedevToken is ERC20Interface{
     
-    function transfer() private override  
+    mapping(address => uint256) private _balances;
+    
+    mapping(address => mapping(address => uint256)) private _allowances;
+    
+    uint 256 private _totalSupply;
+    
+    string private _name;
+    string private _symbol;
+    
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+    
+    
 }
