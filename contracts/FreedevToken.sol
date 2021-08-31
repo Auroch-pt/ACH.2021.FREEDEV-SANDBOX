3f2070ef94fc402a2931404aaf37de553c2ba242
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2021-08-31 18:14:22 +0100

    FreedevToken imp and compiler version changed

--- contracts/FreedevToken.sol
+++ contracts/FreedevToken.sol
@@ -1,36 +1,12 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity ^0.8.0;
 
-interface ERC20Interface {
-    
-    function totalSupply() external view returns (uint);
-    function balanceOf(address tokenOwner) external view returns (uint balance);
-    function transfer(address to, uint tokens) external returns (bool success);
-    
-    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
-    function approve(address spender, uint tokens) external returns (bool success);
-    function transferForm(address from, address to, uint tokens) external returns (bool success);
-    
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract FreedevToken is ERC20Interface{
+contract FreedevToken is ERC20 ("FreeDEV", "FRDV") {
     
-    mapping(address => uint256) private _balances;
-    
-    mapping(address => mapping(address => uint256)) private _allowances;
-    
-    uint 256 private _totalSupply;
-    
-    string private _name;
-    string private _symbol;
-    
-    constructor(string memory name_, string memory symbol_) {
-        _name = name_;
-        _symbol = symbol_;
+    constructor(uint256 initialSupply) {
+        _mint(msg.sender, initialSupply);
     }
-    
-    
 }
--- contracts/issue.sol
+++ contracts/issue.sol
@@ -1,13 +1,13 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity ^0.5.0;
 
 contract Issue {
     
     enum Category {CSS, HTML, NEW_FEATURE, BUG_FIX, REFACTOR, PERFORMANCE_IMPROV, TEST, AUDIT, ARCH_DESIGN }
     enum Field {BACKEND, FRONTEND, BLOCKCHAIN, SQL_DB, AI, BIG_DATA }
     enum Technology {JAVASCRIPT, TYPESCRIPT, NODE_JS, ANGULAR, REACT, SASS, JAVA, C_PLUS_PLUS, C_SHARP, T_SQL, MY_SQL, AWS}
-    enum IssueStatus {OPEN, CLOSED}
+    enum IssueStatus {OPEN, COMPLETED}
     
     uint public id; //Maybe this should be the hash of the block plus the msg.sender, to generate a unique ID, or something similar.
     string public requirement;
@@ -26,11 +26,11 @@ contract Issue {
     
     IssueStatus public status;
     
-    constructor() {
+    constructor(string memory _requirement, string memory _specifications, uint _points) public {
         author = msg.sender;
-    }
-    
-    receive() payable external {
         
+        requirement = _requirement;
+        specifications = _specifications;
+        points = _points;
     }
 }
